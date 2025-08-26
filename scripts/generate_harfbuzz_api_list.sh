31f44479533b10f04abef3ddeb81a5850ab62243
Khaled Hosny <khaled@aliftype.com>
2025-08-27 01:27:02 +0300

    Update full API list

--- scripts/generate_harfbuzz_api_list.sh
+++ scripts/generate_harfbuzz_api_list.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-grep  -e "^hb_" /usr/local/include/harfbuzz/hb*.h  | grep "(" | sed -e 's/ .*$//' | awk -F: '{print $2}' | sort > status/full_api.txt
+
+grep  -e "^hb_" $1/hb*.h  | grep "(" | sed -e 's/ .*$//' | awk -F: '{print $2}' | sort > status/full_api.txt
